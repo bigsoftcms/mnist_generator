de1c653cbfb19480a13d7f1b601920642352ee79
Taehee Lee <kairess87@gmail.com>
2018-10-11 15:56:27 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import keras, datetime
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, GlobalMaxPooling2D, Dense, Reshape, Flatten, Conv2DTranspose
+from keras.models import Model
+from keras.datasets import mnist
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from keras import backend as K
+import numpy as np
+import matplotlib.pyplot as plt
+
+start_time = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+
+# create models
+input_img = Input(shape=(28, 28, 1))
+
+# encoder
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = GlobalMaxPooling2D()(x)
+encoded = Dense(10, activation='softmax', name='encoded')(x)
+
+# reshape
+x = Reshape((1, 1, 10))(encoded)
+
+# decoder
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same', name='decoded')(x)
+
+autoencoder = Model(input_img, [encoded, decoded])
+autoencoder.compile(
+  optimizer=keras.optimizers.Adam(),
+  loss=['categorical_crossentropy', 'binary_crossentropy'],
+  loss_weights=[0.1, 1],
+  metrics=['accuracy']
+)
+
+autoencoder.summary()
+
+# load mnist dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+# tensorboard --logdir=logs
+autoencoder.fit(x_train, [y_train, x_train], epochs=50, batch_size=128, shuffle=True,
+  validation_data=(x_test, [y_test, x_test]), verbose=1,
+  callbacks=[
+    TensorBoard(log_dir='logs/%s' % (start_time)),
+    ModelCheckpoint('./models/%s.h5' % (start_time), monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+  ]
+)
+
+# take a look at the reconstructed digits
+pred = autoencoder.predict(x_test)
+pred_y = pred[0]
+decoded_imgs = pred[1]
+
+n = 10
+plt.figure(figsize=(10, 4), dpi=100)
+for i in range(n):
+  # display original
+  ax = plt.subplot(2, n, i + 1)
+  plt.imshow(x_test[i].reshape(28, 28))
+  plt.gray()
+  ax.set_axis_off()
+
+  # display reconstruction
+  ax = plt.subplot(2, n, i + n + 1)
+  plt.title('%s' % (np.argmax(pred_y[i])))
+  plt.imshow(decoded_imgs[i].reshape(28, 28))
+  plt.gray()
+  ax.set_axis_off()
+
+plt.show()
---
+++ test.py
@@ -0,0 +1,53 @@
+import keras
+from keras.models import load_model
+from keras.models import Model
+from keras.datasets import mnist
+from keras.layers import Input
+import numpy as np
+import matplotlib.pyplot as plt
+
+base_model = load_model('./models/upsample.h5')
+
+base_model.summary()
+
+layers = [l for l in base_model.layers]
+
+# load mnist dataset
+(_, _), (x_test, y_test) = mnist.load_data()
+
+inputs = Input(shape=(10,))
+
+net = None
+for i in range(12, len(layers)):
+  print(layers[i].name)
+  if i == 12:
+    net = layers[i](inputs)
+  else:
+    net = layers[i](net)
+
+# recreate new decoder only model
+model = Model(inputs=inputs, outputs=net)
+model.summary()
+
+input_seed = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+pred = model.predict([keras.utils.to_categorical(input_seed, 10)])
+
+plt.figure(figsize=(10, 4), dpi=100)
+n = len(pred)
+for i, p in enumerate(pred):
+  # display reconstruction
+  ax = plt.subplot(2, n, i + n + 1)
+  plt.title('%s' % (input_seed[i]))
+  plt.imshow(p.reshape(28, 28))
+  plt.gray()
+  ax.set_axis_off()
+
+plt.show()
+
+# new_model = model.layers[12:]
+# model.layers[12].outbound_nodes = []
+# print(new_model[-1].output)
+
+# decoder_model = Model(inputs=inputs, outputs=new_model[-1].output)
+# decoder_model.summary()
